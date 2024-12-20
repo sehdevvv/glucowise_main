560b9c0f88248ec00c45947b70cb6b09aa1c74e9
Sehdev <sehdevsaini36@gmail.com>
2024-12-20 16:09:33 +0530

    Intial commit

---
+++ GlucoWiseTests/GlucoWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoWiseTests.swift
+//  GlucoWiseTests
+//
+//  Created by iOS Development on 04/12/24.
+//
+
+import Testing
+@testable import GlucoWise
+
+struct GlucoWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
